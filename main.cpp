908f8382957780dd9fdf03ed33d26adbe5c64013
komorebi <2110429490@qq.com>
2023-07-02 10:56:36 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include "Tomasulo.h"
 int main() {
-    freopen("superloop.data","r",stdin);
-    freopen("my_reg","w",stdout);
     order_input();
     reset();
     int num=0;
